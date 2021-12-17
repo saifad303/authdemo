9eb8ee9699f9460cfa281916f65bcec322eb6c71
Saif Ahmed <saifad303@gmail.com>
2021-12-18 02:46:41 +0600

    build header component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+import SearchBar from "./SearchBar";
+
+function Header() {
+  return (
+    <div>
+      <Nav />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/SearchBar.js
@@ -0,0 +1,41 @@
+import {
+  SearchIcon,
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/outline";
+
+function SearchBar() {
+  return (
+    <div className=" relative pl-5 pr-20 h-16 w-full bg-[#0c1251] flex items-center text-white">
+      <div className="flex items-center space-x-7">
+        <div className="flex flex-col items-center justify-center">
+          <SearchIcon className="h-6" />
+          <p className="text-[10px]">Search</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <ChevronDownIcon className="h-6" />
+          <p className="text-[10px]">Show KPIS</p>
+        </div>
+      </div>
+      <div className="ml-auto flex flex-col justify-center items-center">
+        <div className="flex items-center space-x-3">
+          <button>
+            <ChevronLeftIcon className="h-6" />
+          </button>
+          <p className="text-xs">Page 1 of 5</p>
+          <button>
+            <ChevronRightIcon className="h-6" />
+          </button>
+        </div>
+        <p className="text-[10px]">Showing 1-30 results</p>
+      </div>
+      <div className=" absolute right-0 top-5 bg-[#b4009d] p-3 rounded rounded-r-none">
+        <PlusCircleIcon className="h-10" />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
